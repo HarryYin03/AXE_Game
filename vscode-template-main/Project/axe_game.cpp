e9851ca154b4e483f849f2a9adbf6397ef5c98f2
HarryYin03 <u6511238@au.edu>
2025-02-04 05:16:19 +0700

    axe_game complete

--- vscode-template-main/Project/axe_game.cpp
+++ vscode-template-main/Project/axe_game.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 
-int main(){
+int main()
+{
     int width{800};
     int height{450};
     InitWindow(width, height, "Axe Game");
@@ -11,7 +12,7 @@ int main(){
     int l_circle_x{circle_x - circle_radius};
     int r_circle_x{circle_x + circle_radius};
     int u_circle_y{circle_y - circle_radius};
-    int d_circle_y{circle_y + circle_radius};
+    int b_circle_y{circle_y + circle_radius};
 
     int direction{10};
 
@@ -21,30 +22,57 @@ int main(){
     int l_axe_x{axe_x};
     int r_axe_x{axe_x + axe_length};
     int u_axe_y{axe_y};
-    int d_axe_y{axe_y + axe_length};
+    int b_axe_y{axe_y + axe_length};
+
+    bool collision_with_axe = 
+                (b_axe_y >= u_circle_y) && 
+                (u_axe_y <= b_circle_y) && 
+                (l_axe_x <= r_circle_x) && 
+                (r_axe_x >= l_circle_x);
 
     SetTargetFPS(60);
-    while(WindowShouldClose() == false){
+    while (WindowShouldClose() == false)
+    {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
-        if(IsKeyDown(KEY_D) && circle_x < width){
-            circle_x += 10;
+        if (collision_with_axe)
+        {
+            DrawText("Game Over", 400, 200, 20, RED);
         }
+        else
+        {
+            l_circle_x = circle_x - circle_radius;
+            r_circle_x = circle_x + circle_radius;
+            u_circle_y = circle_y - circle_radius;
+            b_circle_y = circle_y + circle_radius;
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
 
-        if(IsKeyDown(KEY_A) && circle_x > 25){
-            circle_x -= 10;
-        }
+            collision_with_axe = (b_axe_y >= u_circle_y) && (u_axe_y <= b_circle_y) && (l_axe_x <= r_circle_x) && (r_axe_x >= l_circle_x);
 
-        axe_y += direction;
-        if(axe_y > height || axe_y < 0){
-            direction = -direction;
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+            if (IsKeyDown(KEY_D) && circle_x < width)
+            {
+                circle_x += 10;
+            }
+
+            if (IsKeyDown(KEY_A) && circle_x > 25)
+            {
+                circle_x -= 10;
+            }
+
+            axe_y += direction;
+            if (axe_y > height || axe_y < 0)
+            {
+                direction = -direction;
+            }
         }
 
         EndDrawing();
     }
-
-  
 }
